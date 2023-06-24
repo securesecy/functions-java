d81cd955e021b6f2136bf005a16c3eaf6593c4ea
Krishnaaa21 <115899200+Krishnaaa21@users.noreply.github.com>
2023-06-24 10:53:10 +0530

    Create factorial.java

---
+++ factorial.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Functions {
+
+    public static void PrintFactorial(int n){
+        int factorial=1;
+        for(int i=n ; i>=1; i--){
+            factorial=factorial*i;
+        }
+        System.out.println(factorial);
+        return;
+        
+    }
+	public static void main(String args[]) {
+	    
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+	    PrintFactorial(n);
+	}
+}
