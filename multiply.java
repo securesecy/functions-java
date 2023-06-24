b786d4731260bc56301873abfc749716fe3288ff
Krishnaaa21 <115899200+Krishnaaa21@users.noreply.github.com>
2023-06-24 10:38:43 +0530

    Create multiply.java

---
+++ multiply.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+
+public class Functions {
+
+    public static int Multiply(int a,int b){
+        
+        return a*b;
+        
+    }
+	public static void main(String args[]) {
+	    
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		
+		int result = Multiply(a,b);
+		System.out.println(result);
+	}
+}
